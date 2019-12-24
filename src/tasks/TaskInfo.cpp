24398fc2df37d9b4b7f1c02810ae295095f17496
Jack <->
2019-12-24 12:25:35 +0100

    Use JSON in task files

--- src/tasks/TaskInfo.cpp
+++ src/tasks/TaskInfo.cpp
@@ -4,6 +4,11 @@
 #include <sys/unistd.h>
 #include <sys/file.h>
 
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/filereadstream.h"
+
 #include "TaskInfo.h"
 
 const int PERMISSIONS = 
@@ -28,28 +33,38 @@ TaskStatus* TaskInfo::get_status()
 
 int TaskInfo::get_pid()
 {
-    char buffer[10];
+    using namespace rapidjson;
+ 
+    Document d;
     {
-        int file = open(this->filepath->c_str(), O_RDONLY);
-        int n_bytes = read(file, buffer, 10);
-        close(file);
-        buffer[n_bytes] = 0;
-    }
-    int process_id;
-    {
-        char *_;
-        process_id = strtol(buffer, &_, 10);
+        char readBuffer[65536];
+        FILE* fp = fopen(this->filepath->c_str(), "rb");
+        FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+        d.ParseStream(is);
+        fclose(fp);
     }
 
-    return (int) process_id;
+    int process_id = d["pid"].GetInt();
+
+    return process_id;
 }
 
 
 void create_task_file(char const* home, int task_id, int process_id)
 {
-    const char* task_info_template = "%d";
-    char task_info[10];
-    sprintf(task_info, task_info_template, process_id);
+    const char* contents;
+    rapidjson::StringBuffer buffer;
+    {
+        using namespace rapidjson;    
+        Document d;
+        d.SetObject();
+        Value pid_value;
+        pid_value.SetInt(process_id);
+        d.AddMember("pid", pid_value, d.GetAllocator());
+        Writer<StringBuffer> writer(buffer);
+        d.Accept(writer);    
+        contents = buffer.GetString();
+    }
 
     int info_file;
     {
@@ -59,6 +74,7 @@ void create_task_file(char const* home, int task_id, int process_id)
         info_file_path.append(info_file_name);
         info_file = open(info_file_path.c_str(), O_RDWR | O_CREAT, PERMISSIONS);
     }
-    write(info_file, task_info, strlen(task_info));
+
+    write(info_file, contents, strlen(contents));
     close(info_file);
 }
